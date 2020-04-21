5350ebe90eec640e1f55c880683ea427720da86c
yann1294 <dev.yann12@gmail.com>
2020-04-21 06:44:55 +0530

    Initial commit

---
+++ src/main/java/com/salazard/springframework/springmvc/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.salazard.springframework.springmvc.controllers;
+
+import com.salazard.springframework.springmvc.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model){
+
+        model.addAttribute("products", productService.listAllProducts());
+
+        return "products";
+    }
+}
---
+++ src/main/java/com/salazard/springframework/springmvc/services/ProductService.java
@@ -0,0 +1,10 @@
+package com.salazard.springframework.springmvc.services;
+
+import com.salazard.springframework.springmvc.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> listAllProducts();
+}
